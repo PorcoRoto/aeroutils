eab4d998e75efb1a586fcb9000648e0b0807cfe6
Alex <alex.wm.spm@gmail.com>
2025-07-08 23:38:11 -0600

    starting on interpolating table

--- climbcalculator.py
+++ climbcalculator.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 class TopOfClimbCalculator:
@@ -8,14 +9,34 @@ class TopOfClimbCalculator:
     def ingesttimefueldistancetable(self, filename):
         self.timefueldistancetable = pd.read_csv(filename)
 
-    def setfieldaltitude(self, altitude):
-        self.fieldalt = altitude
+    def setfieldpressurealtitude(self, altitude):
+        self.fieldpressurealt = altitude
+        self.interpolatefieldaltvalues()
 
-    def setcruisealtitude(self, altitude):
-        self.cruisealtitude = altitude
+    def setcruisepressurealtitude(self, altitude):
+        self.cruisepressurealt = altitude
+        
+    def interpolatefieldaltvalues(self):
+        self.fieldtimefromSL = \
+            self.interpolatefrompressurealt(self.fieldpressurealt,
+                                            'timefromSL_min')
+        
+    def interpolatefrompressurealt(self, altitude, tableparameter):
+        interpolatedvalue = np.interp(altitude, list(self.timefueldistancetable['PressureAlt_feet']),
+                                      list(self.timefueldistancetable[tableparameter]))
+        return interpolatedvalue
+        
 
 
 def test_topofclimbcalculator():
-    practictoc = TopOfClimbCalculator()
-    practictoc.ingesttimefueldistancetable('c172_climbtfdtable5-6.csv')
-    assert len(practictoc.timefueldistancetable) == 13
+    practicetoc = TopOfClimbCalculator()
+    practicetoc.ingesttimefueldistancetable('c172_climbtfdtable5-6.csv')
+    assert len(practicetoc.timefueldistancetable) == 13
+    fieldpa = 5355
+    practicetoc.setfieldpressurealtitude(fieldpa)
+    assert practicetoc.fieldpressurealt == fieldpa
+    cruisepa = 8500
+    practicetoc.setcruisepressurealtitude(cruisepa)
+    assert practicetoc.cruisepressurealt == cruisepa
+    practicetoc.interpolatefieldaltvalues()
+    assert practicetoc.fieldtimefromSL == 8.71
