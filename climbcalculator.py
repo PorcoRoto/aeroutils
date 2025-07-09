cb81511268a2e9a3b49beeeedcd37d6e43839ce7
Alex <alex.wm.spm@gmail.com>
2025-07-08 19:12:09 -0600

    added table ingester

---
+++ climbcalculator.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+class TopOfClimbCalculator:
+    def __init__(self):
+        self.timefueldistancetable = pd.DataFrame
+
+    def ingesttimefueldistancetable(self, filename):
+        self.timefueldistancetable = pd.read_csv(filename)
+
+
+def test_topofclimbcalculator():
+    practictoc = TopOfClimbCalculator()
+    practictoc.ingesttimefueldistancetable('c172_climbtfdtable5-6')
+    assert len(practictoc.timefueldistancetable) == 13
