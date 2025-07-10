920da2fe9f0c9837f1309b8a4032a7ab3e78545e
Alex <alex.wm.spm@gmail.com>
2025-07-10 16:18:37 -0600

    got rest of field interp values

--- climbcalculator.py
+++ climbcalculator.py
@@ -4,10 +4,10 @@ import numpy as np
 
 class TopOfClimbCalculator:
     def __init__(self):
-        self.timefueldistancetable = pd.DataFrame
+        self.tfdtable = pd.DataFrame
 
     def ingesttimefueldistancetable(self, filename):
-        self.timefueldistancetable = pd.read_csv(filename)
+        self.tfdtable = pd.read_csv(filename)
 
     def setfieldpressurealtitude(self, altitude):
         self.fieldpressurealt = altitude
@@ -17,13 +17,20 @@ class TopOfClimbCalculator:
         self.cruisepressurealt = altitude
         
     def interpolatefieldaltvalues(self):
-        self.fieldtimefromSL = \
-            self.interpolatefrompressurealt(self.fieldpressurealt,
-                                            'timefromSL_min')
+        self.fieldtime = \
+            np.round(self.interpolatefrompressurealt(self.fieldpressurealt,
+                                            'timefromSL_min'), 3)
+        self.fieldfuel = \
+            np.round(self.interpolatefrompressurealt(self.fieldpressurealt,
+                                            'fuelfromSL_gal'), 3)
+        self.fielddistance = \
+            np.round(self.interpolatefrompressurealt(self.fieldpressurealt,
+                                            'distancefromSL_NM'), 3)
+        
         
     def interpolatefrompressurealt(self, altitude, tableparameter):
-        interpolatedvalue = np.interp(altitude, list(self.timefueldistancetable['PressureAlt_feet']),
-                                      list(self.timefueldistancetable[tableparameter]))
+        interpolatedvalue = np.interp(altitude, list(self.tfdtable['PressureAlt_feet']),
+                                      list(self.tfdtable[tableparameter]))
         return interpolatedvalue
         
 
@@ -31,7 +38,7 @@ class TopOfClimbCalculator:
 def test_topofclimbcalculator():
     practicetoc = TopOfClimbCalculator()
     practicetoc.ingesttimefueldistancetable('c172_climbtfdtable5-6.csv')
-    assert len(practicetoc.timefueldistancetable) == 13
+    assert len(practicetoc.tfdtable) == 13
     fieldpa = 5355
     practicetoc.setfieldpressurealtitude(fieldpa)
     assert practicetoc.fieldpressurealt == fieldpa
@@ -39,4 +46,6 @@ def test_topofclimbcalculator():
     practicetoc.setcruisepressurealtitude(cruisepa)
     assert practicetoc.cruisepressurealt == cruisepa
     practicetoc.interpolatefieldaltvalues()
-    assert practicetoc.fieldtimefromSL == 8.71
+    assert practicetoc.fieldtime == 8.71
+    assert practicetoc.fieldfuel == 1.707
+    assert practicetoc.fielddistance == 10.71
