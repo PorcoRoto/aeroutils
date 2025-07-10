213510b6061b6432920a639587af28ba3df0daa4
Alex <alex.wm.spm@gmail.com>
2025-07-10 16:57:01 -0600

    added climb calcs

--- climbcalculator.py
+++ climbcalculator.py
@@ -15,6 +15,21 @@ class TopOfClimbCalculator:
 
     def setcruisepressurealtitude(self, altitude):
         self.cruisepressurealt = altitude
+        self.interpolatecruisealtvalues()
+        
+    def calculateclimb(self):
+        self.climbtime = np.round(self.cruisetime - self.fieldtime, 3)
+        self.climbfuel = np.round(self.cruisefuel - self.fieldfuel, 2)
+        self.climbdistance = np.round(self.cruisedistance - self.fielddistance, 2)
+        self.climbmaxrate = np.round(np.average([self.cruiseclimbrate, self.fieldclimbrate]), 0)
+        self.climbmaxratespeed = np.round(np.average([self.cruiseclimbspeed, self.fieldclimbspeed]), 1)
+        print(f'Climb values: \n'
+              f'Time: {self.climbtime} min\n'
+              f'Fuel: {self.climbfuel} gal\n'
+              f'Distance: {self.climbdistance} NM\n'
+              f'Max rate of climb is {self.climbmaxrate} at '
+              f'{self.climbmaxratespeed} KIAS')
+    
         
     def interpolatefieldaltvalues(self):
         self.fieldtime = \
@@ -26,6 +41,41 @@ class TopOfClimbCalculator:
         self.fielddistance = \
             np.round(self.interpolatefrompressurealt(self.fieldpressurealt,
                                             'distancefromSL_NM'), 3)
+        self.fieldclimbrate = \
+            np.round(self.interpolatefrompressurealt(self.fieldpressurealt,
+                                            'Rateofclimb_fpm'), 3)
+        self.fieldclimbspeed = \
+            np.round(self.interpolatefrompressurealt(self.fieldpressurealt,
+                                            'Climbspeed_KIAS'), 3)
+        print(f'Field values: \n'
+              f'Time: {self.fieldtime} min\n'
+              f'Fuel: {self.fieldfuel} gal\n'
+              f'Distance: {self.fielddistance} NM\n'
+              f'Max rate of climb is {self.fieldclimbrate} at '
+              f'{self.fieldclimbspeed} KIAS')
+        
+    def interpolatecruisealtvalues(self):
+        self.cruisetime = \
+            np.round(self.interpolatefrompressurealt(self.cruisepressurealt,
+                                            'timefromSL_min'), 3)
+        self.cruisefuel = \
+            np.round(self.interpolatefrompressurealt(self.cruisepressurealt,
+                                            'fuelfromSL_gal'), 3)
+        self.cruisedistance = \
+            np.round(self.interpolatefrompressurealt(self.cruisepressurealt,
+                                            'distancefromSL_NM'), 3)
+        self.cruiseclimbrate = \
+            np.round(self.interpolatefrompressurealt(self.cruisepressurealt,
+                                            'Rateofclimb_fpm'), 3)
+        self.cruiseclimbspeed = \
+            np.round(self.interpolatefrompressurealt(self.cruisepressurealt,
+                                            'Climbspeed_KIAS'), 3)
+        print(f'Cruise values: \n'
+              f'Time: {self.cruisetime} min\n'
+              f'Fuel: {self.cruisefuel} gal\n'
+              f'Distance: {self.cruisedistance} NM\n'
+              f'Max rate of climb is {self.cruiseclimbrate} at '
+              f'{self.cruiseclimbspeed} KIAS')
         
         
     def interpolatefrompressurealt(self, altitude, tableparameter):
@@ -44,8 +94,13 @@ def test_topofclimbcalculator():
     assert practicetoc.fieldpressurealt == fieldpa
     cruisepa = 8500
     practicetoc.setcruisepressurealtitude(cruisepa)
-    assert practicetoc.cruisepressurealt == cruisepa
-    practicetoc.interpolatefieldaltvalues()
+    practicetoc.calculateclimb()
     assert practicetoc.fieldtime == 8.71
     assert practicetoc.fieldfuel == 1.707
     assert practicetoc.fielddistance == 10.71
+    assert practicetoc.cruisetime == 16.0
+    assert practicetoc.cruisefuel == 2.95
+    assert practicetoc.cruisedistance == 20.5
+    assert practicetoc.climbtime == 7.29
+    assert practicetoc.climbfuel == 1.24
+    assert practicetoc.climbdistance == 9.79
