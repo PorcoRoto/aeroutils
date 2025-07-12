767af34e7b21da403686cea7385e5d6690cb2579
Alex <alex.wm.spm@gmail.com>
2025-07-12 11:49:00 -0600

    more work on da, not finished

--- flightutils.py
+++ flightutils.py
@@ -1,6 +1,22 @@
 import unitconversions as uc
 
 
-def calcdensityaltitude(airtempC, stationpressureinHg, dewpoint):
-    airtempK = uc.convertCtoK(airtempC)
-    return airtempK
+def calcdensityaltitude(airtempC, dewpointC, altimetersetting, altitudeft):
+    altitudemeters = uc.feettometers(altitudeft)
+    airtempK = uc.CtoK(airtempC)
+    stationpressureinHg = stationpressure(altimetersetting, altitudemeters)
+    stationpressuremb = uc.inHgtomb(stationpressureinHg)
+    e_vaporpressure = 6.11 * 10 ** ((7.5 * dewpointC) / (273.3 + dewpointC))
+    TvK_virtualTemp = airtempK /\
+        (1 - (e_vaporpressure / stationpressuremb) * (1 - .622))
+    TvR_virtualTemp = uc.KtoR(TvK_virtualTemp)
+    densityaltitudem = 145366 *\
+        (1 - ((17.326 * altimetersetting) / TvR_virtualTemp) ** .235)
+    densityaltitudeft = uc.meterstofeet(densityaltitudem)
+    return densityaltitudeft
+
+
+def stationpressure(altimetersetting, altitudemeters):
+    stationpressure = altimetersetting *\
+        ((288 - 0.0065 * altitudemeters) / 288) ** 5.2561
+    return stationpressure
--- unitconversions.py
+++ unitconversions.py
@@ -21,8 +21,26 @@ def inHgtomb(pressureinHg):
     return pressuremb
 
 
+def KtoR(tempK):
+    tempR = ((9 / 5) * (tempK - 273.15) + 32) + 459.67
+    return tempR
+
+
+def feettometers(feet):
+    meters = .3048 * feet
+    return meters
+
+
+def meterstofeet(meters):
+    feet = meters / .3048
+    return feet
+
+
 def test_unitconversions():
     assert np.round(FtoC(69), 2) == 20.56
     assert np.round(CtoF(69), 2) == 156.2
     assert np.round(CtoK(69), 2) == 342.15
     assert np.round(inHgtomb(30.69), 2) == 1039.28
+    assert np.round(KtoR(69), 2) == 124.2
+    assert np.round(feettometers(69), 2) == 21.03
+    assert np.round(meterstofeet(69), 2) == 226.38
