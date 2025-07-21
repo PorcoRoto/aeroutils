f27ced53bdf4a08b6ab4abc3aae7f171f488b08a
Alex <alex.wm.spm@gmail.com>
2025-07-21 16:47:20 -0600

    toc block works

--- navlogdisplayer.py
+++ navlogdisplayer.py
@@ -13,7 +13,7 @@ class navlogshower:
             self.setinputsfromtest()
         self.calctopofclimb()
         self.generatepressurealttext()
-        self.generatetopofclimbtext
+        self.generatetopofclimbtext()
         self.generatenavlog()
 
     def setinputsfromtest(self):
@@ -42,8 +42,11 @@ class navlogshower:
         
     def generatetopofclimbtext(self):
         self.tocline1 = f'ToC:  time   fuel   dist '
-        self.tocline2 = f'    {self.toc.cruisetime:.1f}   {self.toc.cruisefuel:.1f}   {self.toc.cruisedistance:.1f}'
-        self.tocline3 = f'    {self.toc.fieldtime:.1f}   {self.toc.fieldfuel:.1f}   {self.toc.fielddistance:.1f}'
+        self.tocline2 = f'         {self.toc.cruisetime:.1f}   {self.toc.cruisefuel:.1f}   {self.toc.cruisedistance:.1f}'
+        self.tocline3 = f'          {self.toc.fieldtime:.1f}   {self.toc.fieldfuel:.1f}   {self.toc.fielddistance:.1f}'
+        self.tocline4 = f'         {self.toc.climbtime:.1f}   {self.toc.climbfuel:.1f}   {self.toc.climbdistance:.1f}'
+        self.tocline5 = f'         min   gal   NM'
+        
 
     def generatenavlog(self):
         plt.imshow(self.image)
@@ -57,10 +60,22 @@ class navlogshower:
         line1y = 228
         line2y = 262
         line3y = 298
-        plt.text(pablockx, line1y, self.paline1, fontsize=13)
-        plt.text(pablockx, line2y, self.paline2, fontsize=13)
-        plt.text(pablockx, line3y, self.paline3, fontsize=13)
+        fs = 13
+        plt.text(pablockx, line1y, self.paline1, fontsize=fs)
+        plt.text(pablockx, line2y, self.paline2, fontsize=fs)
+        plt.text(pablockx, line3y, self.paline3, fontsize=fs)
         plt.plot([450, 450], [203, 317], color='black')
+        tocblockx = 460
+        line4y = 334
+        line5y = 370
+        plt.text(tocblockx, line1y, self.tocline1, fontsize=fs)
+        plt.text(tocblockx, line2y, self.tocline2, fontsize=fs)
+        plt.text(tocblockx, line3y, self.tocline3, fontsize=fs)
+        plt.text(tocblockx, line4y, self.tocline4, fontsize=fs)
+        plt.text(tocblockx, line5y, self.tocline5, fontsize=fs)
+        plt.plot([558, 558], [210, 325], color='black')
+        plt.plot([605, 605], [210, 325], color='black')
+        plt.plot([663, 663], [203, 317], color='black')
 
 
 class testinputs:
