417b5702d8c60b3cad8b791ec07f147a336ab838
Alex <alex.wm.spm@gmail.com>
2025-07-26 09:11:07 -0600

    calc pilot line

--- wbtestinput.i
+++ wbtestinput.i
@@ -3,3 +3,5 @@ airframe : R22
 basic empty weight : 875.6
 empty longitudinal arm : 103.87
 empty lateral arm : -.02
+pilot weight : 190
+pilot baggage : 2
--- weightbalmaker.py
+++ weightbalmaker.py
@@ -4,11 +4,14 @@ import numpy as np
 class weightbalance:
     def __init__(self, inputs):
         self.inputs = inputs
-        # self.basicemptyweight = inputs.basicemptyweight
-        # self.emptylongarm = inputs.emptylongarm
-        # self.emptylatarm = inputs.emptylatarm
         self.emptylongmoment = self.calcmoment(self.inputs.basicemptyweight, self.inputs.emptylongarm)
         self.emptylatmoment = self.calcmoment(self.inputs.basicemptyweight, self.inputs.emptylatarm)
+        self.pilotlongmoment = self.calcmoment(
+            (self.inputs.pilotwgt + self.inputs.pilotbags),
+            self.inputs.pilotlongarm)
+        self.pilotlatmoment = self.calcmoment(
+            (self.inputs.pilotwgt + self.inputs.pilotbags),
+            self.inputs.pilotlatarm)
 
     def calcmoment(self, weight, arm):
         moment = np.round(weight * arm, 1)
@@ -22,6 +25,10 @@ class inputreader:
         self.collectlines()
         self.sortlines()
         self.file.close()
+        if self.airframe == 'R22':
+            self.pilotlongarm = 78
+            self.pilotlatarm = 10.7
+            
 
     def sortlines(self):
         for line in self.lines:
@@ -35,6 +42,14 @@ class inputreader:
             self.emptylongarm = float(value)
         elif key == 'empty lateral arm':
             self.emptylatarm = float(value)
+        elif key == 'tail number':
+            self.tailnumber = value
+        elif key == 'airframe':
+            self.airframe = value
+        elif key == 'pilot weight':
+            self.pilotwgt = float(value)
+        elif key == 'pilot baggage':
+            self.pilotbags = float(value)
 
     def getkeyandvaluefromline(self, line):
         if ':' in line:
@@ -45,6 +60,7 @@ class inputreader:
 
     def collectlines(self):
         self.lines = self.file.readlines()
+
         
 
 
@@ -52,5 +68,8 @@ def test_weightbalance():
     inputs = inputreader('wbtestinput.i')
     assert inputs.basicemptyweight == 875.6
     testwb = weightbalance(inputs)
+    assert testwb.inputs.pilotbags == 2
     assert testwb.emptylongmoment == 90948.6
     assert testwb.emptylatmoment == -17.5
+    assert testwb.pilotlongmoment == 14976
+    assert testwb.pilotlatmoment == 2054.4
