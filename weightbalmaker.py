5e37e2394cae763e91fe1cfcb1bff1c8c51fa815
Alex <alex.wm.spm@gmail.com>
2025-07-30 22:49:18 -0600

    refactor

--- weightbalmaker.py
+++ weightbalmaker.py
@@ -5,18 +5,108 @@ class weightbalance:
     def __init__(self, inputs):
         self.inputs = inputs
         self.loadarmdata()
-        self.setbasicemptymoments()
-        self.setpilotmoments()
-        self.setpassengermoments()
-        self.setdoormoments()
-        self.calczerofuelwgt()
-        self.calcfuelwgts()
-        self.calcfuelmoments()
-        self.calctotalwgt()
-        self.calczerofuellongmoment()
-        self.calczerofuellongarm()
-        self.calctotallongarm()
-        self.calclateralarms()
+        self.zerofuelwgtlist = self.buildzerofuelwgtlist()
+        self.zerofuelwgt = self.calczerofuelwgt()
+        self.mainfuelwgt, self.auxfuelwgt = self.calcfuelwgts()
+        self.totalfuelwgtlist = self.buildtotalwgtlist()
+        self.totalwgt = self.calctotalwgt()
+        self.zerolongarmlist, self.totallongarmlist, \
+            self.zerolatarmlist, self.totallatarmlist = \
+            self.buildarmlists()
+        self.zerolongmoment, self.zerolatmoment = self.calczerofuelmoments()
+        self.zerolongarm, self.zerolatarm = self.calczerofuelarms()
+        self.totallongmoment, self.totallatmoment = self.calctotalmoments()
+        self.totallongarm, self.totallatarm = self.calctotalarms()
+        # self.setpilotmoments()
+        # self.setpassengermoments()
+        # self.setdoormoments()
+        
+        # self.calcfuelwgts()
+        # self.calcfuelmoments()
+        
+        # self.calczerofuellongmoment()
+        # self.calczerofuellongarm()
+        # self.calctotallongarm()
+        # self.calclateralarms()
+
+    def calczerofuelwgt(self):
+        zerofuelwgt = np.round(np.sum(self.zerofuelwgtlist), 1)
+        return zerofuelwgt
+
+    def calcfuelwgts(self):
+        mainfuelwgt = self.inputs.mainfuelgal * 6
+        auxfuelwgt = self.inputs.auxfuelgal * 6
+        return mainfuelwgt, auxfuelwgt
+
+    def calctotalwgt(self):
+        totalwgt = np.sum(self.totalfuelwgtlist)
+        return totalwgt
+
+    def buildzerofuelwgtlist(self):
+        zerofuelweightlist =\
+            [self.inputs.basicemptyweight,
+             self.inputs.pilotwgt + self.inputs.pilotbags,
+             self.inputs.passengerwgt + self.inputs.passengerbags,
+             (self.inputs.leftdoor + self.inputs.rightdoor) * self.doorwgt]
+        return zerofuelweightlist
+    
+    def buildarmlists(self):
+        zerolongarmlist = [
+            self.inputs.emptylongarm,
+            self.pilotlongarm,
+            self.passengerlongarm,
+            self.doorlongarm
+        ]
+        totallongarmlist = [
+            0,
+            self.mainfuellongarm,
+            self.auxfuellongarm
+        ]
+        zerolatarmlist = [
+            self.inputs.emptylatarm,
+            self.pilotlatarm,
+            self.passengerlatarm,
+            self.doorlatarm
+        ]
+        totallatarmlist = [
+            0,
+            self.mainfuellatarm,
+            self.auxfuellatarm
+        ]
+        return zerolongarmlist, totallongarmlist, \
+            zerolatarmlist, totallatarmlist
+            
+    def calczerofuelmoments(self):
+        zerolongmoment = np.sum(np.multiply(self.zerofuelwgtlist,
+                                            self.zerolongarmlist))
+        zerolatmoment = np.sum(np.multiply(self.zerofuelwgtlist,
+                                           self.zerolatarmlist))
+        return np.round(zerolongmoment, 1), np.round(zerolatmoment, 1)
+    
+    def calctotalmoments(self):
+        totallongmoment = \
+            np.sum(np.multiply(self.totalfuelwgtlist, self.totallongarmlist)) + \
+                   self.zerolongmoment
+        totallatmoment = \
+            np.sum(np.multiply(self.totalfuelwgtlist, self.totallatarmlist)) + \
+                   self.zerolatmoment
+        return np.round(totallongmoment, 1), np.round(totallatmoment, 1)
+    
+    def calczerofuelarms(self):
+        zerolongarm = self.zerolongmoment / self.zerofuelwgt
+        zerolatarm = self.zerolatmoment / self.zerofuelwgt
+        return np.round(zerolongarm, 2), np.round(zerolatarm, 2)
+    
+    def calctotalarms(self):
+        totallongarm = self.totallongmoment / self.totalwgt
+        totallatarm = self.totallatmoment / self.totalwgt
+        return np.round(totallongarm, 2), np.round(totallatarm, 2)
+    
+    def buildtotalwgtlist(self):
+        totalweightlist = [self.zerofuelwgt,
+                           self.mainfuelwgt,
+                           self.auxfuelwgt]
+        return totalweightlist
 
     def calclateralarms(self):
         self.zerofuellatmoment = np.round(self.emptylatmoment +
@@ -31,7 +121,7 @@ class weightbalance:
         self.totallatarm = np.round(self.totallatmoment / self.totalwgt, 2)
 
     def calctotallongarm(self):
-        self.totallongmoment = np.round(self.zerofuellongmoment +
+        self.totallongmoment = np.round(self.zerolongmoment +
                                         self.mainfuellongmoment +
                                         self.auxfuellongmoment, 1)
         self.totallongarm = np.round(self.totallongmoment / self.totalwgt, 2)
@@ -54,21 +144,6 @@ class weightbalance:
         self.zerofuellongmoment = self.emptylongmoment + self.pilotlongmoment \
             + self.passengerlongmoment + self.doorlongmoment
 
-    def calctotalwgt(self):
-        self.totalwgt = self.zerofuelwgt + self.mainfuelwgt + self.auxfuelwgt
-
-    def calcfuelwgts(self):
-        self.mainfuelwgt = self.inputs.mainfuelgal * 6
-        self.auxfuelwgt = self.inputs.auxfuelgal * 6
-
-    def calczerofuelwgt(self):
-        self.zerofuelwgt = \
-            np.round(self.inputs.basicemptyweight +
-                     self.inputs.pilotwgt + self.inputs.pilotbags +
-                     self.inputs.passengerwgt + self.inputs.passengerbags +
-                     (self.inputs.leftdoor + self.inputs.rightdoor) *
-                     self.doorwgt, 1)
-
     def setdoormoments(self):
         self.doorlongmoment = self.calcmoment(
             (self.inputs.leftdoor + self.inputs.rightdoor) * self.doorwgt,
@@ -78,12 +153,6 @@ class weightbalance:
             (self.inputs.leftdoor + -1 * self.inputs.rightdoor) *
             self.doorlatarm
         )
-
-    def setbasicemptymoments(self):
-        self.emptylongmoment = self.calcmoment(self.inputs.basicemptyweight,
-                                               self.inputs.emptylongarm)
-        self.emptylatmoment = self.calcmoment(self.inputs.basicemptyweight,
-                                              self.inputs.emptylatarm)
 
     def setpilotmoments(self):
         self.pilotlongmoment = self.calcmoment(
@@ -184,27 +253,27 @@ def test_weightbalance():
     assert inputs.basicemptyweight == 875.6
     testwb = weightbalance(inputs)
     assert testwb.inputs.pilotbags == 2
-    assert testwb.emptylongmoment == 90948.6
-    assert testwb.emptylatmoment == -17.5
-    assert testwb.pilotlongmoment == 14976
-    assert testwb.pilotlatmoment == 2054.4
-    assert testwb.passengerlongmoment == 14196
-    assert testwb.passengerlatmoment == -1692.6
-    assert testwb.doorlongmoment == -403
-    assert testwb.doorlatmoment == -109.2
+    # assert testwb.emptylongmoment == 90948.6
+    # assert testwb.emptylatmoment == -17.5
+    # assert testwb.pilotlongmoment == 14976
+    # assert testwb.pilotlatmoment == 2054.4
+    # assert testwb.passengerlongmoment == 14196
+    # assert testwb.passengerlatmoment == -1692.6
+    # assert testwb.doorlongmoment == -403
+    # assert testwb.doorlatmoment == -109.2
     assert testwb.zerofuelwgt == 1244.4
     assert testwb.mainfuelwgt == 60
-    assert testwb.mainfuellongmoment == 6516.0
-    assert testwb.auxfuellongmoment == 3736.8
-    assert testwb.mainfuellatmoment == -660
-    assert testwb.auxfuellatmoment == 403.2
+    # assert testwb.mainfuellongmoment == 6516.0
+    # assert testwb.auxfuellongmoment == 3736.8
+    # assert testwb.mainfuellatmoment == -660
+    # assert testwb.auxfuellatmoment == 403.2
     assert testwb.auxfuelwgt == 36
     assert testwb.totalwgt == 1340.4
-    assert testwb.zerofuellongmoment == 119717.6
-    assert testwb.zerofuellongarm == 96.21
+    assert testwb.zerolongmoment == 119717.6
+    assert testwb.zerolongarm == 96.21
     assert testwb.totallongmoment == 129970.4
     assert testwb.totallongarm == 96.96
-    assert testwb.zerofuellatmoment == 235.0
-    assert testwb.zerofuellatarm == .19
-    assert testwb.totallatmoment == -21.8
+    assert testwb.zerolatmoment == 235.1
+    assert testwb.zerolatarm == .19
+    assert testwb.totallatmoment == -21.7
     assert testwb.totallatarm == -.02
